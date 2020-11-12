ea8b83587c1b34a66bed582995be3816a3b7308b
Giulio <giuliovolletti@hotmail.com>
2020-11-12 16:50:03 +0100

    push dadi

--- dadi/js/dadi.js
+++ dadi/js/dadi.js
@@ -1,4 +1,19 @@
+console.log("Hello World!");
 // Gioco dei dadi, chi fa di più vince.
-// Generare un numero random da 1 a 6, sia per il
-// giocatore sia per il compute. Stabilire il
+// Generare un numero random da 1 a 6
+var nRandom = (Math.random() * 5).toFixed();
+var numCasuale = ++nRandom
+console.log(numCasuale);
+// Generare un numero da 1 a 6, giocatore
+var numGiocatore = (Math.random() * 5).toFixed();
+var numCasualeGiocatore = ++numGiocatore
+console.log(numCasualeGiocatore);
 // vincitore, in base a chi fa il punteggio più alto.
+
+if (numCasuale < numGiocatore ) {
+  document.getElementById('dadi').innerHTML= "numero generato " + numCasuale + " numero giocatore " + numCasualeGiocatore + "<div>hai vinto<div>"
+} else if (numCasuale > numGiocatore) {
+  document.getElementById('dadi').innerHTML= "numero generato " + numCasuale + " numero giocatore " + numCasualeGiocatore + "<div>hai perso<div>"
+} else {
+  document.getElementById('dadi').innerHTML= "numero generato " + numCasuale + " numero giocatore " + numCasualeGiocatore + "<div>hai pareggiato<div>"
+}
