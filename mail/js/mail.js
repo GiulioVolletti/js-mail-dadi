8cc50389834991fd7d4fba106cae2ade1eaa721d
Giulio <giuliovolletti@hotmail.com>
2020-11-12 15:50:26 +0100

    mail push

--- mail/js/mail.js
+++ mail/js/mail.js
@@ -1,3 +1,19 @@
 // Chiedi all’utente la sua email,
+var mailUtente = prompt("inserire qui la tua mail")
+console.log(mailUtente);
+
+// mail di accesso utenti
+var mailConsentite = ["pincopallo@smail.com" , "pdorfigliodikmer@gmail.com" , "anonimo@hotmail.it" , "boh@gmail.com"]
+console.log(mailConsentite);
 // controlla che sia nella lista di chi può accedere,
+for (var accesso = 0; accesso < mailConsentite.length; accesso++) {
+  var verifica = mailConsentite[accesso]
+  console.log(verifica);
+}
 // stampa un messaggio appropriato
+if (verifica = mailUtente ) {
+  document.getElementById('mail').innerHTML= "accesso consentito"
+} else {
+  document.getElementById('mail').innerHTML= "accesso negato"
+}
+console.log(mail);
