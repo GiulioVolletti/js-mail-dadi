d9f0b366c607f4d72c4317b9750b57bfc3a8e3d6
Giulio <giuliovolletti@hotmail.com>
2020-11-12 17:13:40 +0100

    ripush mail

--- mail/js/mail.js
+++ mail/js/mail.js
@@ -1,19 +1,22 @@
+console.log("Hello World!");
 // Chiedi all’utente la sua email,
 var mailUtente = prompt("inserire qui la tua mail")
 console.log(mailUtente);
 
 // mail di accesso utenti
-var mailConsentite = ["pincopallo@smail.com" , "pdorfigliodikmer@gmail.com" , "anonimo@hotmail.it" , "boh@gmail.com"]
+var mailConsentite = ["pincopallo@smail.com" , "pdorfigliodikmer@gmail.com" , "anonimo@hotmail.it" , "boh@gmail.com" ,"prova@gmail.com" ]
 console.log(mailConsentite);
 // controlla che sia nella lista di chi può accedere,
-for (var accesso = 0; accesso < mailConsentite.length; accesso++) {
-  var verifica = mailConsentite[accesso]
-  console.log(verifica);
-}
 // stampa un messaggio appropriato
-if (verifica = mailUtente ) {
-  document.getElementById('mail').innerHTML= "accesso consentito"
-} else {
-  document.getElementById('mail').innerHTML= "accesso negato"
+document.getElementById('mail').innerHTML= "accesso negato"
+
+for (var accesso = 0; accesso < mailConsentite.length; accesso++) {
+  // stampa un messaggio appropriato
+  if (mailUtente === mailConsentite[accesso] ) {
+    document.getElementById('mail').innerHTML= "accesso consentito"
+    console.log(mailConsentite[accesso]);
+  }
 }
+console.log(accesso);
+// console.log(verifica);
 console.log(mail);
